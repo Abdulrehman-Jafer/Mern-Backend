91e9ff82cced8f11a48c83d9110d580717c30a79
Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>
2023-02-07 13:59:29 +0500

    Added .env File

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ import mongoose from "mongoose";
 import cors from "cors"
 import router from "./routes/user-route";
 import blogRouter from "./routes/blog-routes";
+import dotenv from "dotenv"
 
 const app = express();
 app.use(express.json());
-// Understood Unable to use Api in react app running at different port without the cors
 app.use(cors())
 app.use("/api/user", router)
 app.use("/api/blog",blogRouter)
-
+dotenv.config()
 mongoose
-  .connect(
-    "mongodb+srv://*********:*********@node-cluster.e3ibxci.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => app.listen(5000))
+  .connect(process.env.MONGO_URL)
+  .then(() => app.listen(process.env.PORT || 8000))
   .then(() => console.log("LISTENING AT THE PORT 5000"))
   .catch((err) => console.log(err));
