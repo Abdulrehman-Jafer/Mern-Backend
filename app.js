ffdc48e7a26da454620e6d3d488acc4d4f39f77f
Abdulrehman-Jafer <abdulrehmanjaferworks01233@gmail.com>
2023-06-22 20:19:49 +0500

    added vercel config

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ import dotenv from "dotenv"
 const app = express();
 app.use(express.json());
 app.use(cors())
+dotenv.config()
 
 app.use("/api/user", userRouter)
 app.use("/api/blog",blogRouter)
-dotenv.config()
+
+app.use("/",(req,res,next)=>{
+res.send("<h1>Welcome to my blogging app API</h1>")
+  next()
+})
+
 mongoose.set('strictQuery',true)
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URI)
   .then(() => app.listen(process.env.PORT || 8000))
   .then(() => console.log(`LISTENING AT THE PORT ${process.env.PORT}`))
   .catch((err) => console.log(err));
